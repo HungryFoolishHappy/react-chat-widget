f6fc71fd9e941e9d8c61834b8d5ab2e46a0dedf6
Jack C <rosemarydia2023@gmail.com>
2023-10-08 23:08:42 +0800

    add lang

--- dev/App.tsx
+++ dev/App.tsx
@@ -1,14 +1,30 @@
 import { Component } from 'react';
 
-import { Widget, addResponseMessage, setQuickButtons, toggleMsgLoader, addLinkSnippet } from '../index';
+import { toggleWidget, Widget, addResponseMessage, setQuickButtons, toggleMsgLoader, addLinkSnippet } from '../index';
 import { addUserMessage } from '..';
+import './styles.scss';
+
+class Language {
+  name: string;
+  value: string;
+  constructor(name, value) {
+    this.name = name;
+    this.value = value;
+  }
+}
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+    let full = false;
+  }
+
   componentDidMount() {
     addResponseMessage('Welcome to this awesome chat!');
     addLinkSnippet({ link: 'https://google.com', title: 'Google' });
     addResponseMessage('![](https://raw.githubusercontent.com/Wolox/press-kit/master/logos/logo_banner.png)');
     addResponseMessage('![vertical](https://d2sofvawe08yqg.cloudfront.net/reintroducing-react/hero2x?1556470143)');
+    toggleWidget();
   }
 
   handleNewUserMessage = (newMessage: any) => {
@@ -35,18 +51,35 @@ export default class App extends Component {
     }
     return true;
   }
+  
+  
+
+  R() {
+    const LANGUAGE_LIST = [
+      new Language('English','en'),
+      new Language('繁體中文','zh'),
+      new Language('简体中文','cn')
+    ];
+    return (
+      <div className="rcw-dropdown-menu">
+          <select className="rcw-select">
+            {LANGUAGE_LIST.map(language => <option key={language.name} value={language.value}>{language.name}</option>)}
+          </select>
+        </div>
+    )
+  }
 
   render() {
     return (
       <Widget
-        title="Bienvenido"
-        subtitle="Asistente virtual"
+        fullScreenMode
+        title="Bienvenido Bienvenido Bienvenido Bienvenido"
+        subtitle="Asistente virtual Asistente virtual Asistente virtual"
         senderPlaceHolder="Escribe aquí ..."
         handleNewUserMessage={this.handleNewUserMessage}
         handleQuickButtonClicked={this.handleQuickButtonClicked}
         imagePreview
         handleSubmit={this.handleSubmit}
-        emojis
       />
     );
   }
